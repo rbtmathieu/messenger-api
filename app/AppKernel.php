468a9c184d3b88b5757587f55a8a27c9da413f6f
Mathieu <mathieurob60@gmail.com>
2016-02-14 22:31:14 +0100

    Add FosRestBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
             new MessengerBundle\MessengerBundle(),
             new UserBundle\UserBundle(),
         );
---
+++ src/MessengerBundle/Controller/MessagesController.php
@@ -0,0 +1,13 @@
+<?php
+namespace MessengerBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+class MessagesController extends FOSRestController
+{
+    public function getUsersAction()
+    {
+        // @TODO: Change this shit later
+        dump('It works!!');die;
+    }
+}
